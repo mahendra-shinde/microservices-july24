d1eabf7df1e3cb2fb549805a158a884a0c8a7b96
Mahendra Shinde <MahendraShinde@synegertics-india.com>
2024-07-17 17:12:16 +0530

    config server1

---
+++ config-server/src/test/java/com/mahendra/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahendra.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
